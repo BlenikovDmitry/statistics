406493ee1737d1f55ca36dc1167b9fb8bec9fafc
Dmitriy <158054275+BlenikovDmitry@users.noreply.github.com>
2024-03-01 19:48:55 +0300

    Add files via upload
    Whats new:
    
    - add bootstap as a function
    - read .csv file as a function

--- lib.py
+++ lib.py
@@ -1,5 +1,33 @@
 import random
 
+"""
+Функция потерь
+попарно берет разницу между значениями, возводит в квадрат и берет среднее
+Так мы узнаем, насколько сильно различаются прогнозные и фактические данные
+
+!!!ВАЖНО размер выборок должен быть одинаков и обе выборки должны быть порождены одним процессом
+Иначе в функции нет смысла
+Подаем на вход:
+- plan - прогнозный результат
+- fact - фактический результат
+
+Возвращает среднее - насколько различаются выборки
+
+"""
+def lost_function(plan, fact):
+    diff = 0
+    average = 0
+    counter = 0
+    
+    while counter < len(plan):
+        diff = plan[counter] - fact[counter]
+        diff = diff * diff
+        average = average + diff
+        counter = counter + 1
+    average = average / len(plan)
+    
+    return average
+
 #Функция выборки с возвратом
 #Берет данные и маркеры из генеральной совокупности
 #в случайном порядке
@@ -77,11 +105,16 @@ def selection_without_return_without_markers(data, data_var, elems):
         data.remove(data[int(tmp)])
         counter = counter + 1
 
-
+"""
+Копирует один массив в другой
+"""
 def memcpy(data_old,data_new):
     for elems in data_old:
         data_new.append(elems)
-
+"""
+Подсчитывает суммы повторно выбранных групп
+для проверки статистической гипотезы о случайности значений в группах
+"""
 def selection_groups_sel_statictics(data, data_all):
     data_var = []
     data_tmp = []
@@ -99,3 +132,68 @@ def selection_groups_sel_statictics(data, data_all):
         data_tmp = []
         data_var = []
     return sums_tmp
+
+"""
+считывает данные из файла in.csv
+записывает в два массива:
+- markers - маркеры
+- data - данные
+"""
+def read(data, markers):
+    with open("in.csv", 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            markers.append(str(row[0]))
+            data.append(float(row[1]))
+"""
+Бутстрап
+делает 10.000 итераций , формирует распределение среднего, медианы, стандартного отклонения
+отрисовывает распределение среднего, стандартного отклонения, выборку
+"""
+def bootstrap(data, markers):
+    counter_iteration = 0
+    counter = 0
+    tmp = 0
+    distribution_average = []
+    distribution_median = []
+    distribution_stde = []
+    while counter_iteration < 10000:
+        counter = 0
+        markers_var = []
+        data_var = []
+        while counter < len(markers):
+            tmp = random.random() * len(data)
+            data_var.append(data[int(tmp)])
+            markers_var.append(markers[int(tmp)])
+            counter = counter + 1
+
+      
+        average = statistics.mean(data_var)
+    
+        median = statistics.median(data_var)
+ 
+        stde = statistics.stdev(data_var)
+       
+        distribution_average.append(average)
+        distribution_median.append(median)
+        distribution_stde.append(stde)
+    
+        
+        counter_iteration = counter_iteration + 1
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(2,2,1)
+    ax.set_title("Распределение среднего")
+    ax.hist(distribution_average, 50, color = "red", ec="red")
+    ax.grid()
+    
+    ax = fig.add_subplot(2,2,2)
+    ax.set_title("Распределение стандартного отклонения")
+    ax.hist(distribution_stde, 50, color = "lightblue", ec="red")
+    ax.grid()
+
+    ax = fig.add_subplot(2,2,3)
+    ax.set_title("Выборка")
+    ax.plot(markers, data, color = "lightblue")
+    ax.grid()
+
+    plt.show()
