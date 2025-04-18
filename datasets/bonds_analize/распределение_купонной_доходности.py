8a2d896a4a6bc2aa034090f41c1d371d2d9f4c37
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2025-04-18 11:25:20 +0300

    Add files via upload

--- datasets/bonds_analize/распределение_купонной_доходности.py
+++ datasets/bonds_analize/распределение_купонной_доходности.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import csv
 import statistics
+import numpy as np
 
 """
 Скрипт считает распределение доходностей в облигациях, строит гистограмму
 Также делает простейший анализ(считает среднее ,медиану ,моду), чтобы показать , около каких значений группировка доходностей
 Для входа нужен файл .csv формата
-<название> <код> <число бумаг> <купонная доходность>
-купонная доходность уже должна быть приведена к цене покупки в портфель
+<название> <цена> <число бумаг> <купонная доходность>
 
 Принцип действия:
 - считываем данные
@@ -15,47 +15,103 @@ import statistics
 - строим гистограмму
 
 """
+#название бумаги
+bonds_name = []
+#цена бумаги
+bonds_price = []
+#число бумаг
 bonds_count = []
-percents_coupon = []
+#номинал бумаги
+nominal = []
+#купон в рублях
+coupon = []
+#число выплат в год
+coupon_count = []
 """
 чтение данных в структуры данных
 """
-with open('russian_bonds.csv', 'r', newline='') as csvfile:
+with open('russian_bonds.csv', 'r', newline='',encoding="utf-8") as csvfile:
     data_reader = csv.reader(csvfile, delimiter=';')
     for row in data_reader:
+        bonds_name.append(str(row[0]))
+        bonds_price.append(str(row[1]))
         bonds_count.append(str(row[2]))
-        percents_coupon.append(str(row[3]))
+        nominal.append(str(row[3]))
+        coupon.append(str(row[4]))
+        coupon_count.append(str(row[5]))
+#убираем заголовок
+bonds_name.pop(0)
+bonds_price.pop(0)
+bonds_count.pop(0)
+nominal.pop(0)
+coupon.pop(0)
+coupon_count.pop(0)
+
+'''print(bonds_name)
+print(bonds_price)
+print(bonds_count)
+print(nominal)
+print(coupon)
+print(coupon_count)'''
+
 """
 ////////////////////////////////////////////////////
 """
-bonds_count.pop(0)
-percents_coupon.pop(0)
 
+#делаем приведение типов и считаем цену покупки с учетом номинала и цены на бирже
 counter = 0
 while counter < len(bonds_count):
     bonds_count[counter] = int(bonds_count[counter])
-    percents_coupon[counter] = float(percents_coupon[counter])
+    coupon_count[counter] = int(coupon_count[counter])
+    coupon[counter] = float(coupon[counter])
+    bonds_price[counter] = float(bonds_price[counter]) * int(nominal[counter]) / 100
     counter += 1
-
+#сюда собираем все купоны в % для анализа и отрисовки на гистограмму
 result = []
+
+#считаем купонные выплаты в % и заталкиваем в result
+'''
+Д - текущая доходность
+Д = К / Ц * 100%, где:
+
+К — сумма купонных выплат за период;
+Ц — текущая рыночная цена облигации.
+Подробнее на сайте Banki.ru https://www.banki.ru/news/daytheme/?id=10982360
+в нашем случае:
+К = coupon[counter_upper] * coupon_count[counter_upper](купон * число выплат в год)
+Ц = bonds_price[counter_upper](цена бумаги на момент покупки с учетом рыночной цены в % поправленной на номинал покупки)
+'''
 counter_upper = 0
 counter = 0
-
 while counter_upper < len(bonds_count):
     counter = 0
     while counter < bonds_count[counter_upper]:
-        result.append(percents_coupon[counter_upper])
+        result.append(((coupon[counter_upper] * coupon_count[counter_upper]) / bonds_price[counter_upper]) * 100)
         counter += 1
     counter_upper += 1
+#центральное положение
+print("среднее:" + str(round(statistics.mean(result),2)))
+print("медиана:" + str(statistics.mode(result)))
+print("мода:" + str(statistics.median(result)))
 
-print("среднее:" + str(statistics.mean(percents_coupon)))
-print("медиана:" + str(statistics.mode(percents_coupon)))
-print("мода:" + str(statistics.median(percents_coupon)))
+#после анализа приводим к int, формируем шкалу по оси X и отрисовывааем на графике, сохраняя в отдельный файл
+counter = 0
+while counter < len(result):
+    result[counter] = int(result[counter])
+    counter += 1
 
-fig, ax = plt.subplots()
+
+x_ticks = []
+counter = min(result)
+while counter < max(result):
+    x_ticks.append(counter)
+    counter += 1
+
+fig, ax = plt.subplots(figsize=(25,10))
 plt.title("Распределение купонов")
 plt.xlabel("Купонная доходность")
 plt.ylabel("Число купленных бумаг")
-ax.hist(result, bins=len(bonds_count), linewidth=2, color = 'green', edgecolor="black", rwidth = 5)
+plt.xticks(x_ticks)
+ax.hist(result,bins=len(bonds_count), linewidth=2, color = 'green', edgecolor="black", rwidth = 5)
 ax.grid()
-plt.show()
+plt.savefig("interest_distribution.png")
