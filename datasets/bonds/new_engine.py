f11329087d2cc6bd72ffe970b51f1091db4cb377
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2025-04-21 19:45:48 +0300

    Add files via upload

--- datasets/bonds/new_engine.py
+++ datasets/bonds/new_engine.py
@@ -61,7 +61,7 @@ file_name = str(file_name)
 file_name = file_name[2:10]
 
 #выводим в файл все данные очищенные
-ofz.to_csv('test/site/' + file_name +'ofz_cleared.csv', encoding = 'Windows-1251')
+ofz.to_csv('result/site/' + file_name +'ofz_cleared.csv', encoding = 'Windows-1251')
 
 #фильтруем данные по доходности последней сделки  и выводим в отдельный файл
 #доходность последней сделки должна быть больше нуля и меньше 2-х стандартных отклонений, чтобы отсечь выбросы
@@ -70,7 +70,7 @@ selected_data = selected_data[selected_data['Дох посл сделки'] < se
                                                                                                                     'Сделок шт.', 'Дох посл сделки','Ставка купона',
                                                                                                                     'Объем в валюте', 'Дата погашения']]
 #выводим данные в файл
-selected_data.to_csv('test/site/' + file_name+'ofz_cleared_short.csv', encoding = 'Windows-1251')
+selected_data.to_csv('result/site/' + file_name+'ofz_cleared_short.csv', encoding = 'Windows-1251')
 
 #подсчет статистик по очищенным данным
 #создаем кадр данных и подсчитываем статистики
@@ -79,7 +79,7 @@ selected_data.to_csv('test/site/' + file_name+'ofz_cleared_short.csv', encoding 
 statistics = pd.DataFrame(columns=['Дата','Средняя ставка купона', 'Отклонение средней ставки купона','Средняя доходность последней сделки', 'Отклонение доходности последней сделки'
                                    ,'Средняя цена', 'Отклонение средней цены', 'Объем торгов в рублях'])
 
-statistics = pd.read_csv('test/result_ofz.csv', encoding = 'Windows-1251')
+statistics = pd.read_csv('result_ofz.csv', encoding = 'Windows-1251')
 print(statistics)
 statistics.loc[file_name] = [file_name, round(selected_data['Ставка купона'].mean(), 2),round(selected_data['Ставка купона'].std(), 2),
                        round(selected_data['Дох посл сделки'].mean(),2), round(selected_data['Дох посл сделки'].std(),2),round(selected_data['Цена % средневзвешенная'].mean(),2),
@@ -91,7 +91,7 @@ print(duplicates)
 
 
 #выводим в файл статистики
-statistics.to_csv('test/result_ofz.csv', encoding = 'Windows-1251', index = False)
+statistics.to_csv('result_ofz.csv', encoding = 'Windows-1251', index = False)
 
 '''
 генерация графиков
@@ -106,8 +106,8 @@ plt.ylabel("Купон")
 
 plt.xticks(rotation="vertical")
 ax.plot(selected_data['Наименование'],selected_data['Ставка купона'], '-rh', linewidth=3, markersize=5, markerfacecolor='b')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'kupon_ofz.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'kupon_ofz.png')
 #дох посл сделки несортированная
 fig, ax = plt.subplots(figsize=(25,10))
 ax.set_title("Доходность общая по последней сделке")
@@ -115,8 +115,8 @@ plt.xlabel("Выпуск")
 plt.ylabel("Доходность последней сделки")
 plt.xticks(rotation="vertical")
 ax.plot(selected_data['Наименование'],selected_data['Дох посл сделки'], '-gh', linewidth=3, markersize=5, markerfacecolor='b')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'dohod_eff_ofz.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'dohod_eff_ofz.png')
 #цена несортированная
 fig, ax = plt.subplots(figsize=(25,10))
 ax.set_title("Цена")
@@ -124,8 +124,8 @@ plt.xlabel("Выпуск")
 plt.ylabel("Цена")
 plt.xticks(rotation="vertical")
 ax.plot(selected_data['Наименование'],selected_data['Цена % средневзвешенная'], '-bh', linewidth=3, markersize=5, markerfacecolor='b')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'price_ofz.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'price_ofz.png')
 
 
 #отрисовываем распределение купонов
@@ -133,8 +133,8 @@ fig, ax = plt.subplots(figsize=(25,10))
 plt.xticks(rotation="vertical")
 ax.set_title("Распределение % купона по выпускам")
 ax.bar(selected_data['Наименование'], selected_data['Ставка купона'], linewidth = 3.0, color = 'blue', ec='red')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'interest_distribution_ofz.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'interest_distribution_ofz.png')
 #цена сортированная
 selected_data = selected_data.sort_values(by='Цена % средневзвешенная', ascending=False)
 fig, ax = plt.subplots(figsize=(25,10))
@@ -143,8 +143,8 @@ plt.xlabel("Выпуск")
 plt.ylabel("Цена")
 plt.xticks(rotation="vertical")
 ax.plot(selected_data['Наименование'],selected_data['Цена % средневзвешенная'], '-bh', linewidth=3, markersize=5, markerfacecolor='b')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'price_ofz_sorted.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'price_ofz_sorted.png')
 #купонная доходность сортированная
 selected_data = selected_data.sort_values(by='Ставка купона', ascending=False)
 fig, ax = plt.subplots(figsize=(25,10))
@@ -154,8 +154,8 @@ plt.ylabel("Купон")
 
 plt.xticks(rotation="vertical")
 ax.plot(selected_data['Наименование'],selected_data['Ставка купона'], '-rh', linewidth=3, markersize=5, markerfacecolor='b')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'kupon_ofz_sorted.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'kupon_ofz_sorted.png')
 #график погашения сортированный
 selected_data['Дата погашения'] = pd.to_datetime(selected_data['Дата погашения'], format='mixed')
 selected_data = selected_data.sort_values(by='Дата погашения', ascending=False)
@@ -163,8 +163,8 @@ fig, ax = plt.subplots(figsize=(25,10))
 plt.xticks(rotation="vertical")
 ax.set_title("График погашения")
 ax.plot(selected_data['Наименование'],selected_data['Дата погашения'], '-bh', linewidth=3, markersize=5, markerfacecolor='b')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'end_graphic_ofz.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'end_graphic_ofz.png')
 
 #дох посл сделки сортированная
 selected_data = selected_data.sort_values(by='Дох посл сделки', ascending=False)
@@ -174,8 +174,8 @@ plt.xlabel("Выпуск")
 plt.ylabel("Доходность последней сделки")
 plt.xticks(rotation="vertical")
 ax.plot(selected_data['Наименование'],selected_data['Дох посл сделки'], '-gh', linewidth=3, markersize=5, markerfacecolor='b')
-ax.grid()
-plt.savefig('test/site/' + file_name + 'dohod_eff_ofz_sorted.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'dohod_eff_ofz_sorted.png')
 
 #объем торгов
 selected_data['Объем в валюте'] = selected_data['Объем в валюте'].astype(float)
@@ -185,13 +185,13 @@ ax = fig.add_subplot(1,1,1)
 plt.xticks(rotation="vertical")
 ax.set_title("Объем торгов")
 ax.bar(selected_data['Наименование'], selected_data['Объем в валюте'], width=1, color = "lightblue", edgecolor="red", linewidth=0.7)
-ax.grid()
-plt.savefig('test/site/' + file_name + 'volume_ofz_sorted.png')
+ax.grid(which = 'both')
+plt.savefig('result/site/' + file_name + 'volume_ofz_sorted.png')
 
 '''
 генерация страницы отчета
 '''
-file_out = 'test/' + file_name + '.html'
+file_out = 'result/' + file_name + '.html'
 gen.init(file_name, file_out)
 
 gen.statistic_header(file_out)
