96402dcd964a8312ef252ea57c30943b16d90707
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2025-05-12 16:11:11 +0300

    Update Py2PDF_read.py

--- trash/Py2PDF/Py2PDF_read.py
+++ trash/Py2PDF/Py2PDF_read.py
@@ -1,15 +1,29 @@
 import PyPDF2
+
 '''
-пример чтения файла pdf
-Совместим с последней версией Py2PDF(3.0.0)
+считываем данные из файла pdf
+пишем в текстовый файл
 '''
+
+#чтение файла
 pdf = open('example.pdf', 'rb')
+#создаем объект reader
 pdf_reader = PyPDF2.PdfReader(pdf)
+#получаем число страниц
 num_pages = len(pdf_reader.pages)
 
+with open("out.txt", "w", encoding="utf-8") as file:
+    file.write('')
+
+with open('out.txt', 'w') as f:
+    f.write('')
+
+#в цикле выводим все страницы
 for page in range(num_pages):
     pdf_page = pdf_reader.pages[page]
-    print(pdf_page.extract_text())
-
+    pdf_extr = pdf_page.extract_text()
+    with open("out.txt", "a", encoding="utf-8") as file:
+        file.write(pdf_extr)
+    
 
 pdf.close()
