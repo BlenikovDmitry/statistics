1326e7c98ad70000dfb03dd53eb3175739de967b
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2025-05-12 16:28:06 +0300

    Update Py2PDF_read.py

--- trash/Py2PDF/Py2PDF_read.py
+++ trash/Py2PDF/Py2PDF_read.py
@@ -2,9 +2,10 @@ import PyPDF2
 
 '''
 считываем данные из файла pdf
-пишем в текстовый файл
+пишем в текстовый файл чтобы разбить построчно
+считываем полученный файл и работать нужно будет уже с ним
 '''
-
+raw_data = []
 #чтение файла
 pdf = open('example.pdf', 'rb')
 #создаем объект reader
@@ -24,6 +25,16 @@ for page in range(num_pages):
     pdf_extr = pdf_page.extract_text()
     with open("out.txt", "a", encoding="utf-8") as file:
         file.write(pdf_extr)
-    
+
 
 pdf.close()
+    
+f = open('out.txt',encoding="utf-8")
+for line in f:
+    raw_data.append(line)
+print(raw_data)
+
+counter = 0
+while counter < 40:
+    print(raw_data[counter])
+    counter += 1
