7b916749ced27187a204811e1e61bae44854d81b
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2026-04-09 16:32:54 +0300

    Add files via upload

--- trash/Fast API/example_simple_get.py
+++ trash/Fast API/example_simple_get.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Path
 from fastapi.responses import HTMLResponse, PlainTextResponse, FileResponse
 
 # как запустить веб  сервер
-# fastapi dev ex1.py
+# fastapi dev example_simple_get.py
 
 
 app = FastAPI()
@@ -16,7 +16,12 @@ def root():
                     <h2> /text : запросу отдает простой текст </h2>
                     <h2> /text1 : будто бы то же самое, что и text, но вернет он тут Json по умолчанию </h2>
                     <h2> /text2 : здесь явно указываем, что отдаем текст </h2>
-                    <h2> /cb.csv : отдача файла с сервера </h2>'''
+                    <h2> /cb.csv : отдача файла с сервера </h2>
+                    <h2> /path_params/{id_} : get запрос с 1 числовым параметром </h2>
+                    <h2> /path_params_text/{id_}/{text} : get запрос с 1 числовым и 1 текстовым параметром </h2>
+                    <h2> /path_params_adv/{text} : get запрос с параметрами, возвращает текстовый параметh, есть встроенная валидация на длину не менее 3 и не более 20 символов </h2>
+                    <h2> /path_params_adv_id/{id_} : get запрос с параметрами, возвращает числовой параметр есть встроенная валидация на длину больше или равно 0 </h2>
+                    '''
     return HTMLResponse(content=html_content)
     return {'message':'Hello World!'}
 #создаем ресурс hello и по запросу к ресурсу отдаем html
@@ -51,3 +56,24 @@ def get_some_file():
         filename='cb.csv', 
         media_type='text/csv'  # Явно указываем тип файла
     )
+#get запрос с параметрами, возвращает числовой параметр,
+#есть встроенная валидация
+@app.get('/path_params/{id_}')
+def path_params(id_: int):
+    return {'id':id_}
+#get запрос с параметрами, возвращает числовой параметр и текстовый
+#есть встроенная валидация
+@app.get('/path_params_text/{id_}/{text}')
+def path_params(id_: int, text: str):
+    return {'id':id_, 'text': text}
+#get запрос с параметрами, возвращает текстовый параметр
+#есть встроенная валидация на длину не менее 3 и не более 20 символов
+#все благодаря классу Path
+@app.get('/path_params_adv/{text}')
+def path_params(text: str = Path(min_length = 3, msx_length = 20)):
+    return {'text': text}
+#get запрос с параметрами, возвращает числовой параметр
+#есть встроенная валидация на длину больше или равно 0
+@app.get('/path_params_adv_id/{id_}')
+def path_params(id_: int = Path(ge = 0)):
+    return {'text': id_}
