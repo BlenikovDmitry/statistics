1338224f049a386eb04f8300948ac6f4c51e290d
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2026-04-10 13:52:45 +0300

    Доработки
    Перенаправил выгрузку результатов в папку result и графиков в graphics

--- datasets/cb/ks_infl.py
+++ datasets/cb/ks_infl.py
@@ -2,87 +2,102 @@ import pandas as pd
 import gc
 import matplotlib.pyplot as plt
 from bootstraping import bootstrap
+import streamlit as st
+import sys
+#загрузчик файла сырых данных
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is None:
+    sys.exit("Программа завершена")
+
+#считывание сырых данных для обработки    
+raw_set = pd.read_csv(uploaded_file, encoding = 'Windows-1251', sep=';')
 
 
-
-'''
-считали данные
-почистили данные
-сжали по используемой памяти
-сделали срез
-очистили память от лишних данных
-'''
-
-print('Запускаем анализ...')
-print('Читаю файл cb.csv...')
-raw_set = pd.read_csv('cb.csv', encoding = 'Windows-1251', sep=';')
-
-print('Подготовка данных для анализа...')
+#очистка и преобразование типов данных
 raw_set['Дата'] = raw_set['Дата'].astype('string')
-
 raw_set['Ключевая ставка, % годовых'] = raw_set['Ключевая ставка, % годовых'].str.replace(',', '.')
 raw_set['Инфляция, % г/г'] = raw_set['Инфляция, % г/г'].str.replace(',', '.')
-
-
 raw_set['Ключевая ставка, % годовых'] = raw_set['Ключевая ставка, % годовых'].astype('float32')
 raw_set['Инфляция, % г/г'] = raw_set['Инфляция, % г/г'].astype('float32')
-
 raw_set['Ключевая ставка, % годовых'] = raw_set['Ключевая ставка, % годовых'].round(2)
 raw_set['Инфляция, % г/г'] = raw_set['Инфляция, % г/г'].round(2)
-
 final_set = raw_set.loc[:, ['Дата', 'Ключевая ставка, % годовых', 'Инфляция, % г/г']]
 
 del raw_set
 gc.collect()
+#интерфейс заголовка и подсчет корреляции по Пирсону и описательный анализ
+st.header("Анализ ключевой ставки ЦБ и инфляции")
+st.subheader("Корелляция ключевой ставки и инфляции")
 
-'''
-общий описательный анализ ключевой ставки и инфляции
-c записью в файл desc.csv
-попутно считаем корелляцию по Пирсону
-c записью в файл correliation.csv
-
-'''
-print('Рассчитываю коэффициент корреляции по методу Пирсона...')
-correliation = final_set[['Ключевая ставка, % годовых', 'Инфляция, % г/г']].corr(method='pearson')
-correliation.to_csv('correliation.csv', encoding = 'Windows-1251')
-print('Вывожу матрицу корреляции:')
-print(correliation)
-print('Продублировал в файл correliation.csv')
-print('Провожу разведочный анализ...')
+correliation = round(final_set[['Ключевая ставка, % годовых', 'Инфляция, % г/г']].corr(method='pearson').iloc[0,1],2)
+#st.write(correliation)
+if correliation == 0:
+    st.write('Нет связи на исследуемом промежутке')
+if correliation > 0:
+    st.write('При росте инфляции растет ключевая ставка')
+    st.write('При падении инфляции падает ключевая ставка')
+if correliation < 0:
+    st.write('При росте инфляции падает ключевая ставка')
+    st.write('При падении инфляции растет ключевая ставка')
+    
+    
 desc = final_set.describe(include = ['float32']).astype('float32').round(2)
-desc.to_csv('desc.csv', encoding = 'Windows-1251')
-print('Вывожу результат разведочного анализа:')
-print(desc)
-print('Продублировал в файл desc.csv')
+st.subheader("Описательный анализ")
+
+desc = desc.rename(index = {
+    'count': 'Число наблюдений',
+    'mean': 'Среднее',
+    'std': 'Отклонение от среднего',
+    'min': 'Минимум',
+    'max': 'Максимум',
+    '50%': 'Медиана'
+
+    })
+
+st.write(desc)
 del desc
 del correliation
 gc.collect()
-'''
-строим графики кс и инфляции, накладывая их друг на друга
-генерируем результат в файл graphic.png
-также вызываем бутстрап из отдельного файла, который возвращает график
-графики сохраняем в отдельных файлах
-'''
 
+#график наложенных друг на друга инфляции и ключевой ставки
+st.header("Графики значений и графики распределения КС и инфляции")
 fig, ax = plt.subplots(figsize=(25,10))
 plt.title('Ключевая ставка / Инфляция')
 plt.xlabel("месяц.год")
 plt.ylabel("Ставка")
 
-print('Строю график ключевой ставки и инфляции...')
 plt.xticks(rotation="vertical")
 ax.plot(final_set['Дата'], final_set['Ключевая ставка, % годовых'], '-bo', linewidth=2, markersize=6, markerfacecolor='black')
 ax.plot(final_set['Дата'], final_set['Инфляция, % г/г'], '-ro', linewidth=2, markersize=6, markerfacecolor='black')
 plt.legend(['Ключевая ставка, % годовых', 'Инфляция, % г/г'])
 ax.grid()
-plt.savefig('graphic.png')
-print('Готово! Результат записан в файл graphic.png')
-print('Провожу бутстрап ключевой ставки, чтобы найти наиболее вероятную в ближайшем будущем...')
+st.pyplot(fig)
+plt.close()
+
+#график бутстрапа ключевой ставки и стандартного отклонения чтобы понять распределение
+st.subheader("Ключевая ставка: ")
 count_iterations = 10000
-plt = bootstrap(final_set['Ключевая ставка, % годовых'], count_iterations, 'ks')
-plt.savefig('ks.png')
-print('Готово! Результат записан в файл ks.png')
-print('Провожу бутстрап инфляции, чтобы найти наиболее вероятную в ближайшем будущем...')
-plt = bootstrap(final_set['Инфляция, % г/г'], count_iterations, 'infl')
-plt.savefig('infl.png')
-print('Готово! Результат записан в файл infl.png')
+distribution_average, distribution_stde, average, stde = bootstrap(final_set['Ключевая ставка, % годовых'], count_iterations)
+fig = plt.figure(figsize=(6,6))
+ax = fig.add_subplot(2,1,1)
+ax.set_title("Распределение среднего ключевой ставки")
+ax.hist(distribution_average, 50, color = "red", ec="lightblue", edgecolor="black", rwidth = 5)
+plt.axvline(average,linewidth=4, color='g', label = "Среднее изначальное")
+plt.legend()
+ax.grid()
+
+st.pyplot(fig)
+plt.close()
+#график бутстрапа инфляции и стандартного отклонения чтобы понять распределение
+st.subheader("Инфляция: ")
+distribution_average, distribution_stde, average, stde = bootstrap(final_set['Инфляция, % г/г'], count_iterations)
+fig = plt.figure(figsize=(6,6))
+ax = fig.add_subplot(2,1,1)
+ax.set_title("Распределение среднего инфляции")
+ax.hist(distribution_average, 50, color = "green", ec="lightblue", edgecolor="black", rwidth = 5)
+plt.axvline(average,linewidth=4, color='r', label = "Среднее изначальное")
+plt.legend()
+ax.grid()
+
+st.pyplot(fig)
+plt.close()
