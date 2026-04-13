fb2934e857398091ecc4a14df9da48d59dbdac94
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2026-04-13 21:09:11 +0300

    Новые фичи
    Перенес сюда функцию выгрузки в json файл из скрипта export_cb_data.py
    Переписал функцию parse_to_csv, чтобы она парсила любой файл json с одинаковой структурой

--- datasets/cb/parse_json.py
+++ datasets/cb/parse_json.py
@@ -1,12 +1,12 @@
 import json
 import pandas as pd
 '''
-Функция парсит json файл дл депозитов
+Функция парсит json файл данных
 - загружает файл .json
 - вытаскивает оттуда два датафрейма - данные и обозначения
 - схлопывает и записывает в отдельный файл
 '''
-def parse_deposit_to_csv(path):
+def parse_to_csv(path):
     with open(path, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
@@ -18,9 +18,18 @@ def parse_deposit_to_csv(path):
     df_merged = df_merged[['obs_val', 'dt', 'elname']]
     df_merged.rename(columns = {'obs_val': 'Значение',
                                 'dt': 'Период',
-                                'elname': 'Вид депозита'}, inplace = True)
+                                'elname': 'Вид'}, inplace = True)
     df_merged.to_csv(path[:-4] + 'csv', encoding='utf8', index = False)
     return 'Json преобразован в файл .csv: ' + path[:-4] + 'csv'
 
-
-#parse_deposit_to_csv()
+'''
+Вспомогательная функция чтобы не было дублирования кода
+сохраняет файл json если запрос успешен
+'''
+def write_raw_json(path, req):
+    if req.status_code == 200:
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump(req.text, f,ensure_ascii=False, indent=4)
+        return {'Данные загружены в файл json' }
+    else:
+        return req.status_code
