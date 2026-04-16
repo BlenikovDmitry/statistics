5fa4a97e9550813d0b8c75aca4702d4dbc0b45f0
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2026-04-16 16:48:02 +0300

    Фичи
    Добавил главную страницу и отдачу страниц на каждый ендпоинт

--- datasets/cb/export_cb_data.py
+++ datasets/cb/export_cb_data.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import requests
 import pandas as pd
 import json
+from fastapi.responses import FileResponse
 from parse_json import parse_to_csv, write_raw_json, create_graphic
 '''
 ендпоинты для получения данных с сайта ЦБ РФ
-+ распарсить json в dataframe
-- подумать над статистическими показателями
 '''
     
 app = FastAPI()
@@ -15,11 +15,12 @@ app = FastAPI()
 @app.get('/deposit/{y1}/{y2}')
 def export_deposit_interest(y1:int, y2:int):
     req = requests.get(f'https://www.cbr.ru/dataservice/data?y1={y1}&y2={y2}&publicationId=18&datasetId=37&measureId=2')
+    print(req.status_code)
     print( write_raw_json(f'deposit/from_{y1}_to_{y2}.json', req))
     print( parse_to_csv(f'deposit/from_{y1}_to_{y2}.json'))
     create_graphic(f'deposit/from_{y1}_to_{y2}.csv')
-    
-    return { 'Данные успешно загружены' } 
+    #в будущем написать отдельную страницу
+    return FileResponse('public/index.html') 
 
 #2) Средневзвешенная ставка по ипотечным жилищным кредитам, предоставленным физическим лицам-резидентам
 #https://www.cbr.ru/dataservice/data?y1=2026&y2=2026&publicationId=14&datasetId=29&measureId=
@@ -29,8 +30,8 @@ def export_mortgage_interest(y1:int, y2:int):
     print( write_raw_json(f'mortgage/from_{y1}_to_{y2}.json', req))
     print( parse_to_csv(f'mortgage/from_{y1}_to_{y2}.json'))
     create_graphic(f'mortgage/from_{y1}_to_{y2}.csv')
-    
-    return { 'Данные успешно загружены' }
+    #в будущем написать отдельную страницу
+    return FileResponse('public/index.html')
     
 
 #3) Средневзвешенные процентные ставки по кредитам, предоставленным кредитными организациями физическим лицам
@@ -41,8 +42,8 @@ def export_credit_interest(y1:int, y2:int):
     print (write_raw_json(f'credit/from_{y1}_to_{y2}.json', req))
     print( parse_to_csv(f'credit/from_{y1}_to_{y2}.json'))
     create_graphic(f'credit/from_{y1}_to_{y2}.csv')
-    
-    return { 'Данные успешно загружены' }
+    #в будущем написать отдельную страницу
+    return FileResponse('public/index.html')
 
 
 #4) Задолженность по кредитам, предоставленным физическим лицам-резидентам
@@ -53,8 +54,8 @@ def export_credit_debt(y1:int, y2:int):
     print( write_raw_json(f'credit_debt/from_{y1}_to_{y2}.json', req)) 
     print( parse_to_csv(f'credit_debt/from_{y1}_to_{y2}.json'))
     create_graphic(f'credit_debt/from_{y1}_to_{y2}.csv')
-    
-    return { 'Данные успешно загружены' }
+    #в будущем написать отдельную страницу
+    return FileResponse('public/index.html')
 
 #41) Просроченная задолженность по кредитам, предоставленным физическим лицам-резидентам
 #https://www.cbr.ru/dataservice/data?y1=2026&y2=2026&publicationId=20&datasetId=43&measureId=22
@@ -64,5 +65,11 @@ def export_credit_overdue_debt(y1:int,y2:int):
     print( write_raw_json(f'credit_overdue_debt/from_{y1}_to_{y2}.json', req))
     print( parse_to_csv(f'credit_overdue_debt/from_{y1}_to_{y2}.json'))
     create_graphic(f'credit_overdue_debt/from_{y1}_to_{y2}.csv')
-    
-    return { 'Данные успешно загружены' }
+    #в будущем написать отдельную страницу
+    return FileResponse('public/index.html')
+
+#установка главной страницы приложения
+#потом переписать через явной енпоинт / и явную отдачу файла
+#и впредь не смей монтировать каталоги с heml страницами
+#монтируй только ресурсы(звуки, стили ,нюдсы и тд)
+app.mount('/', StaticFiles(directory='public', html=True), name = 'static')
