432c1f3bb62049ab228ee4b235a0e998fe9fe4d9
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2026-05-15 18:54:19 +0300

    Добавил исследований нормального распределения

--- trash/check_normal.py
+++ trash/check_normal.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 df = pd.read_csv('bonds.csv', encoding = 'windows-1251')
 
@@ -19,10 +21,37 @@ def statistic(df, field):
 
     return (mean, median, mode, desc)
 
+#функция построения квантиль - квантильного графика
+#сравниваем с нормальным распределением
+def qqplot(df, field):
+    plt.figure(figsize = (6,4))
+    stats.probplot(df[field], dist="norm", plot=plt)
 
+    plt.title("Q-Q график (Проверка на нормальность)")
+    plt.xlabel("Теоретические квантили")
+    plt.ylabel("Выборочные квантили")
+    plt.grid(True)
+    plt.show()
+    
+#подсчет z оценок каждого из элементов
+def z_scores(df, field):
+    return stats.zscore(df[field])
+
+#проверяем. сколько точек лежит вне 95% нормальности
+def check_outliers(df, field):
+    z_scores1 = z_scores(df, field)
+    z_np2 = np.sum((z_scores1 > 1.96) | (z_scores1 < -1.96))
+    return (z_np2 / df[field].sum()) * 100
+
+print(str(check_outliers(df, 'Сумма купона')) + "%")
+
+    
+df = df[df['Сумма купона'] < 100]
+
+qqplot(df,'Сумма купона')
 #отрисовка графика распределения(гистограмма)
 #подсчет сигм и количества точек, которые туда попадают(до 20 сигм)
-#квантиль - квантильный график
+#диаграмма рассеяния
 #оценка хвостов - метод хилла
 
 #другие показатели нормальности распределения
