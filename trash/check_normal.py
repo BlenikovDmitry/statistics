80aa77d5e191c2f8f0ef0965accb7df9cbd34680
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2026-05-20 20:12:51 +0300

    Новые фичи
    Подсчет значений около сигм
    Диаграмма рассеивания

--- trash/check_normal.py
+++ trash/check_normal.py
@@ -9,7 +9,7 @@ df['Сумма купона'] = df['Сумма купона'].str.replace('-','0
 df['Сумма купона'] = df['Сумма купона'].str.replace(',','.')
 df['Сумма купона'] = df['Сумма купона'].astype(np.float32)
 
-print(df['Сумма купона'])
+#print(df['Сумма купона'])
 
 #функция подсчета среднего, медианы, моды
 #и разведочного анализа
@@ -41,18 +41,34 @@ def z_scores(df, field):
 def check_outliers(df, field):
     z_scores1 = z_scores(df, field)
     z_np2 = np.sum((z_scores1 > 1.96) | (z_scores1 < -1.96))
-    return (z_np2 / df[field].sum()) * 100
+    return (z_np2 / df[field].count()) * 100
 
-print(str(check_outliers(df, 'Сумма купона')) + "%")
+#функция считает по сигмам
+#если в сумме не 100%, значит есть пропуски в столбце
+def count_sigmas(df,field):
+    std1 = df[field].std()
+    df['sigma'] = abs(df[field] - std1)
+    sigma1 = df[(df['sigma'] < std1)]
+    sigma2 = df[(df['sigma'] > std1) & (df['sigma'] < std1 * 2)]
+    sigma3 = df[(df['sigma'] > std1 * 2) & (df['sigma'] < std1 * 3)]
+    sigma4 = df[(df['sigma'] > std1 * 3) & (df['sigma'] < std1 * 4)]
+    sigma5 = df[(df['sigma'] > std1 * 4) & (df['sigma'] < std1 * 5)]
+    sigma5_1 = df[(df['sigma'] > std1 * 5)]
+    print(f' В пределах 1 сигмы {sigma1.shape[0] / df.shape[0] * 100} %')
+    print(f' В пределах 2 сигм {sigma2.shape[0] / df.shape[0] * 100} %')
+    print(f' В пределах 3 сигм {sigma3.shape[0] / df.shape[0] * 100} %')
+    print(f' В пределах 4 сигм {sigma4.shape[0] / df.shape[0] * 100} %')
+    print(f' В пределах 5 сигм {sigma5.shape[0] / df.shape[0] * 100} %')
+    print(f' Более 5 сигм: {sigma5_1.shape[0] / df.shape[0] * 100} %')
 
-    
-df = df[df['Сумма купона'] < 100]
+#диаграмма рассеивания - нужно протестить
+#если x = y - можно понять, есть ли разрывы в данных и сходу сгруппировать визуально
+def scatter(df, field, df1, field1):
+    plt.figure(figsize = (6,4))
+    plt.scatter(df[field], df1[field1], c = 'red', edgecolors = 'black')
+    plt.title('Зависимость')
+    plt.show()
 
-qqplot(df,'Сумма купона')
-#подсчет сигм и количества точек, которые туда попадают(до 5 сигм)
-#для этого нужно посчитать отклонение каждой точки
-#и сгруппировать по сигмам
-#диаграмма рассеяния
 
 #оценка хвостов - метод хилла
 #отдельно нарисовать график qqplot для хвоста - чтобы понять,  можем ли использовать метод хилла
